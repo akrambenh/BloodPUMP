571d929c3fe9275258a9ade3eeba8038749bf736
akram <akramb.blinx@gmail.com>
2023-03-13 23:10:03 +0100

    setting database

--- app/src/main/java/com/example/bloodbump/MainActivity.java
+++ app/src/main/java/com/example/bloodbump/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.bloodbump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login_button, signup_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        login_button = (Button) findViewById(R.id.login_button);
+        signup_button = (Button) findViewById(R.id.signup_button);
     }
+    public void login(View view){
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+    public void signup(View view){
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
 }
