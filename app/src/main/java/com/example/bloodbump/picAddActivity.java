b1df7afff9f0ab0debaccf3a79a299bb110e2b71
akram <akramb.blinx@gmail.com>
2023-04-05 05:59:04 +0000

    added profile picture uploader from album and camera

--- app/src/main/java/com/example/bloodbump/picAddActivity.java
+++ app/src/main/java/com/example/bloodbump/picAddActivity.java
@@ -2,6 +2,7 @@ package com.example.bloodbump;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +22,9 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,18 +32,39 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.sql.Blob;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.getstream.avatarview.AvatarView;
 
 public class picAddActivity extends AppCompatActivity {
+    private FirebaseAuth userAuth;
+    private FirebaseStorage storage;
     private Button skip_continue_button;
+    private File photoFile = null;
+    private String currentPhotoPath = null;
     private Button take_pic_button;
     private Button upload_pic_button;
     private CircleImageView profile_image;
-    public Bitmap user_bitmap;
+    private Bitmap user_bitmap = null;
+    private String imageDatabasePath;
+    private static final int CAMERA_REQUEST = 1888;
     public static final int PICK_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +72,41 @@ public class picAddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pic_adding);
         skip_continue_button = (Button) findViewById(R.id.skip_continue_button);
         profile_image = (CircleImageView) findViewById(R.id.profile_image);
+        userAuth = FirebaseAuth.getInstance();
+        //String UID = userAuth.getCurrentUser().getUid().toString();
+        String UID = "Akram";
+        skip_continue_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(photoFile == null){
+                    // set standard avatar as profile pic and save in storage
+                }else {
+                    storage = FirebaseStorage.getInstance();
+                    StorageReference storageRef = storage.getReferenceFromUrl("gs://bloodbump-35398.appspot.com/");
+                    StorageReference avatarRef = storageRef.child("Avatar/" + UID + "/" + photoFile.getName());
+                    Bitmap bitmap = ((BitmapDrawable) profile_image.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
+                    UploadTask uploadTask = avatarRef.putBytes(data);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(picAddActivity.this, "Failed To Upload Image", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(picAddActivity.this, "Image Uplaoded Successfully", Toast.LENGTH_SHORT).show();
+                            imageDatabasePath = taskSnapshot.getMetadata().getPath();
+                            Toast.makeText(picAddActivity.this, imageDatabasePath, Toast.LENGTH_SHORT).show();
+                            // Put Image Path to Database for later calling
+                        }
+                    });
+
+                }
+            }
+        });
     }
     public void PicOptions(View view) {
         Toast.makeText(this, "Opening Pic Options", Toast.LENGTH_SHORT).show();
@@ -60,7 +119,8 @@ public class picAddActivity extends AppCompatActivity {
         take_pic_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent CameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(CameraIntent, CAMERA_REQUEST);
             }
         });
         upload_pic_button.setOnClickListener(new View.OnClickListener() {
@@ -78,7 +138,6 @@ public class picAddActivity extends AppCompatActivity {
         dialog.show();
 
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,14 +146,29 @@ public class picAddActivity extends AppCompatActivity {
             try {
                 user_bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
                 //Bitmap roundPic = getRoundedCornerBitmap(user_bitmap);
+                skip_continue_button.setText("Continue");
                 profile_image.setImageBitmap(user_bitmap);
+                photoFile = createImageFile();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
+            Toast.makeText(this, currentPhotoPath, Toast.LENGTH_SHORT).show();
+        }if(requestCode == CAMERA_REQUEST){
+            user_bitmap = (Bitmap) data.getExtras().get("data");
+            skip_continue_button.setText("Continue");
+            profile_image.setImageBitmap(user_bitmap);
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Toast.makeText(this, currentPhotoPath, Toast.LENGTH_SHORT).show();
         }
-        // Used This Method to manually make photo's corners Rounder now with CircleImageView Dependency this method is not needed
-    }public Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+
+
+    }
+    // Used This Method to manually make photo's corners Rounder now with CircleImageView Dependency this method is not needed
+    public Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
         Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
                 bitmap.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
@@ -111,5 +185,20 @@ public class picAddActivity extends AppCompatActivity {
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         canvas.drawBitmap(bitmap, rect, rect, paint);
         return output;
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        //uploadUri = image.toURI();
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
