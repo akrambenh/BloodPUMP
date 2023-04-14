d891fe3db2999da5a26475de43c85cc1ca048cca
akram <akramb.blinx@gmail.com>
2023-04-14 08:58:32 +0100

    Added map,appointment will be added soon

---
+++ app/src/main/java/com/example/bloodbump/PlacesFragemnt.java
@@ -0,0 +1,255 @@
+package com.example.bloodbump;
+
+import static android.content.ContentValues.TAG;
+
+import static com.google.firebase.database.core.RepoManager.clear;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class PlacesFragemnt extends Fragment implements  OnMapReadyCallback{
+    private GoogleMap FragmentMap;
+    private SupportMapFragment supportMapFragment;
+    private static final float NEARBY_ZOOM = 14f;
+    private FusedLocationProviderClient client;
+    private double currentLat;
+    private double currentLong;
+    private Button TitleSelection;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_places_fragemnt, null, false);
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            supportMapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, supportMapFragment).commit();
+        supportMapFragment.getMapAsync(this);
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        // Inflate the layout for this fragment
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+        return view;
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //when permission granted
+                //call method
+                getCurrentLocation();
+            }
+        }
+    }
+    private void moveCamera(LatLng latLng, float zoom, String title){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        FragmentMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        MarkerOptions option = new MarkerOptions()
+                .position(latLng)
+                .title(title);
+        FragmentMap.addMarker(option);
+    }
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //when success
+                if (location != null){
+                    currentLat = location.getLatitude();
+                    currentLong = location.getLongitude();
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            FragmentMap = googleMap;
+                            moveCamera(new LatLng(currentLat, currentLong),
+                                    NEARBY_ZOOM, "My Location");
+                            Nearby();
+                            FragmentMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                                @Override
+                                public boolean onMarkerClick(@NonNull Marker marker) {
+                                    TitleSelection = getActivity().findViewById(R.id.titleSelection);
+                                    TitleSelection.setVisibility(View.VISIBLE);
+                                    TitleSelection.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            String Marker = marker.getTitle();
+
+                                        }
+                                    });
+                                    return false;
+                                }
+                            });
+
+                        }
+                    });
+                }else
+                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            }
+        });
+    }
+    public void Nearby(){
+        String venue = this.getArguments().getString("venue").toLowerCase();
+        String radius = this.getArguments().getString("radius");
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                "?location=" + currentLat + "," + currentLong +
+                "&radius=" + radius +
+                "&name=" + venue +
+                "&region=DZ" +
+                "&key=AIzaSyA5hxDEk36Yub71bI9FrJt1HfCW9tz1_9k";
+        new PlaceTask().execute(url);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+    @SuppressLint("StaticFieldLeak")
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                data = downloadUrl(strings[0]);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            new ParserTask().execute(s);
+        }
+    }
+    private String downloadUrl(String string) throws IOException {
+        URL url = new URL(string);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+        while ((line = reader.readLine()) != null){
+            builder.append(line);
+        }
+        String data = builder.toString();
+        reader.close();
+        return data;
+    }
+    @SuppressLint("StaticFieldLeak")
+    private class ParserTask extends  AsyncTask<String,Integer, List<HashMap<String,String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            JsonParser jsonParser = new JsonParser();
+            List<HashMap<String,String>> mapList = null;
+            JSONObject object = null;
+            try {
+                object = new JSONObject(strings[0]);
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            FragmentMap.clear();
+            for (int i=0; i<hashMaps.size(); i++){
+                HashMap<String,String> hashMapList = hashMaps.get(i);
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                String name = hashMapList.get("name");
+                LatLng latlong = new LatLng(lat,lng);
+                MarkerOptions options = new MarkerOptions();
+                options.position(latlong);
+                options.title(name);
+                FragmentMap.addMarker(options);
+            }
+            HashMap<String, String> hashMapList = hashMaps.get(0);
+            double camLat = Double.parseDouble(hashMapList.get("lat"));
+            double camLng = Double.parseDouble(hashMapList.get("lng"));
+            LatLng camlatlng = new LatLng(camLat, camLng);
+            FragmentMap.moveCamera(CameraUpdateFactory.newLatLngZoom(camlatlng, NEARBY_ZOOM));
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/bloodbump/SearchVenueActivity.java
+++ app/src/main/java/com/example/bloodbump/SearchVenueActivity.java
@@ -1,10 +1,18 @@
 package com.example.bloodbump;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -13,7 +21,15 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class SearchVenueActivity extends AppCompatActivity {
+    private double currentLat;
+    private double currentLong;
+    private FusedLocationProviderClient client;
+    private SupportMapFragment supportMapFragment;
     private EditText searchEditText;
     private RadioGroup venueGroup;
     private RadioButton hospitalRadio;
@@ -54,5 +70,15 @@ public class SearchVenueActivity extends AppCompatActivity {
     }
 
     public void SearchNearby(View view) {
+        Bundle bundle = new Bundle();
+        int ChosenID = venueGroup.getCheckedRadioButtonId();
+        RadioButton ChosenButton = findViewById(ChosenID);
+        String venue = ChosenButton.getText().toString();
+        bundle.putString("venue", venue);
+        String radius  = radiusText.getText().toString();
+        bundle.putString("radius", radius);
+        PlacesFragemnt fragVenue = new PlacesFragemnt();
+        fragVenue.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.searchResult_content, fragVenue).commit();
     }
 }
